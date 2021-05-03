7b9a69a92645f399b089b0630a44cc1ab3721052
nachito <44816790+Kisotyvevo@users.noreply.github.com>
2021-05-03 17:12:21 -0300

    .

---
+++ mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QEventLoop>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_Run_clicked()
+{
+    system("wmic process where name=\"ShaderCompileWorker.exe\" CALL setpriority 32768");
+    ui->RunBoost->setText("On");
+
+}
+
+void MainWindow::on_Stop_clicked()
+{
+    system("wmic process where name=\"ShaderCompileWorker.exe\" CALL setpriority 32");
+    ui->RunBoost->setText("Off");
+}
