ce46922912f2b6102a0eb782ed45bcd433e38767
nachito <44816790+Kisotyvevo@users.noreply.github.com>
2021-05-06 05:36:40 -0300

    .

---
+++ Verificadores/Verificador.bat
@@ -0,0 +1,8 @@
+@echo off
+:start
+cls
+timeout /t 2
+tasklist /fi "imagename eq ShaderCompileWorker.exe" | find /i "ShaderCompileWorker.exe" > nul
+if not errorlevel 1 ("Equide">%cd%\Equide.txt) else (del "%cd%\Equide.txt")
+tasklist /fi "imagename eq Boost.exe" | find /i "Boost.exe" > nul
+if not errorlevel 1 (goto start) else (exit)
---
+++ Verificadores/Verificador.vbs
@@ -0,0 +1,1 @@
+CreateObject("Wscript.Shell").Run "Verificador.bat", 0, True
